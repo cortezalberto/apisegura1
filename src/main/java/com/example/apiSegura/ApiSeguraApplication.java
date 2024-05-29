0a8017f166c2000bd1e18337911c609df73a30bc
cortezalberto <cortezalberto@gmail.com>
2024-05-29 14:06:53 -0300

    Primer commit!

---
+++ src/main/java/com/example/apiSegura/ApiSeguraApplication.java
@@ -0,0 +1,14 @@
+package com.example.apiSegura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSeguraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSeguraApplication.class, args);
+		System.out.println("FUncionando la seguridad de Alberto");
+	}
+
+}
---
+++ src/test/java/com/example/apiSegura/ApiSeguraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiSegura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSeguraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
